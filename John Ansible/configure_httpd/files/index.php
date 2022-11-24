159b8daff66c43ae925755a2f1fdd12c28f7f6a2
john-btg <71494892+john-btg@users.noreply.github.com>
2022-11-24 17:20:05 +0100

    Add files via upload

---
+++ John Ansible/configure_httpd/files/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo$_SERVER['SERVER_NAME']; ?></title>
+    </head>
+    <body>
+        Nom du serveur :
+        <?php
+            echo$_SERVER['SERVER_NAME'];
+        ?>
+    </body>
+</html>
